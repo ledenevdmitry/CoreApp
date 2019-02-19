d40287df303664ae1df6a30f11f76550a42b5962
dledenev <dimzan18@yandex.ru>
2019-02-19 17:18:12 +0300

    Сделал C и Z

--- CoreApp/OraUtils/ZPatchDAL.cs
+++ CoreApp/OraUtils/ZPatchDAL.cs
@@ -1,44 +1,44 @@
-﻿using System;
+﻿using Oracle.ManagedDataAccess.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Oracle.ManagedDataAccess.Client;
 
 namespace CoreApp.OraUtils
 {
-    class CPatchDAL
+    class ZPatchDAL
     {
         private static string insertScript =
-            "insert into сpatch_log" +
-            "( cpatch_id,  parent_id,  release_id,  cpatch_name,    date,  dwsact)" +
+            "insert into zpatch_log" +
+            "( zpatch_id,  parent_id,  release_id,  zpatch_name,    date,  dwsact)" +
             "values" +
-            "(:cpatch_id, :parent_id, :release_id, :cpatch_name, sysdate, 'I');" +
+            "(:zpatch_id, :parent_id, :cpatch_id, :zpatch_name, sysdate, 'I');" +
             "insert into сpatch" +
-            "( cpatch_id,  parent_id,  release_id,  cpatch_name)" +
+            "( zpatch_id,  parent_id,  cpatch_id,  zpatch_name)" +
             "values" +
-            "(:cpatch_id, :parent_id, :release_id, :cpatch_name);" ;
+            "(:zpatch_id, :parent_id, :cpatch_id, :zpatch_name);";
 
         private static string updateScript =
             "insert into сpatch_log" +
-            "( cpatch_id,  parent_id,  release_id,  cpatch_name,    date,  dwsact)" +
+            "( zpatch_id,  parent_id,  cpatch_id,  zpatch_name,    date,  dwsact)" +
             "values" +
-            "(:cpatch_id, :parent_id, :release_id, :cpatch_name, sysdate, 'U');" +
-            "update сpatch" +
+            "(:zpatch_id, :parent_id, :cpatch_id, :zpatch_name, sysdate, 'U');" +
+            "update zpatch" +
             "set " +
             "parent_id = :parent_id," +
-            "release_id = :release_id" +
-            "cpatch_name = :cpatch_name)" +
+            "cpatch_id = :cpatch_id" +
+            "zpatch_name = :zpatch_name)" +
             "where" +
-            "cpatch_id = :cpatch_id;";
+            "zpatch_id = :zpatch_id;";
 
         private static string deleteCPatchScript =
             "insert into сpatch_log" +
-            "( cpatch_id,  parent_id,  release_id,  cpatch_name,    date,  dwsact)" +
+            "( zpatch_id,  parent_id,  cpatch_id,  zpatch_name,    date,  dwsact)" +
             "values" +
-            "select cpatch_id, parent_id, release_id, cpatch_name, sysdate, 'D'" +
-            "from cpatch " +
-            "where cpatch_id = :cpatch_id;" +
+            "select zpatch_id, parent_id, cpatch_id, zpatch_name, sysdate, 'D'" +
+            "from zpatch " +
+            "where zpatch_id = :zpatch_id;" +
             "delete from сpatch" +
             "where " +
             "cpatch_id = :cpatch_id;";
@@ -55,52 +55,52 @@ namespace CoreApp.OraUtils
             "cpatch_name = :cpatch_name" +
             "cpatch_id = :cpatch_id);";
 
-        public static void Insert(int cpatch_id, int parent_id, int release_id, int cpatch_name)
+        public static void Insert(int zpatch_id, int parent_id, int cpatch_id, int cpatch_name)
         {
             OracleTransaction transaction = DBManager.BeginTransaction();
             DBManager.ExecuteNonQuery(
                 insertScript,
                 transaction,
-                new OracleParameter("cpatch_id"  , cpatch_id),
-                new OracleParameter("parent_id"  , parent_id),
-                new OracleParameter("release_id" , release_id),
+                new OracleParameter("zpatch_id", zpatch_id),
+                new OracleParameter("parent_id", parent_id),
+                new OracleParameter("cpatch_id", cpatch_id),
                 new OracleParameter("cpatch_name", cpatch_name));
             transaction.Commit();
         }
 
-        public static void Update(int cpatch_id, int parent_id, int release_id, int cpatch_name)
+        public static void Update(int zpatch_id, int parent_id, int cpatch_id, int cpatch_name)
         {
             OracleTransaction transaction = DBManager.BeginTransaction();
             DBManager.ExecuteNonQuery(
                 updateScript,
                 transaction,
-                new OracleParameter("cpatch_id", cpatch_id),
+                new OracleParameter("cpatch_id", zpatch_id),
                 new OracleParameter("parent_id", parent_id),
-                new OracleParameter("release_id", release_id),
+                new OracleParameter("release_id", cpatch_id),
                 new OracleParameter("cpatch_name", cpatch_name));
             transaction.Commit();
         }
 
-        public static void DeleteCPatch(int cpatch_id)
+        public static void DeleteZPatch(int zpatch_id)
         {
             OracleTransaction transaction = DBManager.BeginTransaction();
             DBManager.ExecuteNonQuery(
                 deleteCPatchScript,
                 transaction,
-                new OracleParameter("cpatch_id", cpatch_id));
+                new OracleParameter("zpatch_id", zpatch_id));
             transaction.Commit();
         }
 
-        public static void DeleteDependency(int cpatch_id, int parent_id, int release_id, int cpatch_name)
+        public static void DeleteDependency(int zpatch_id, int parent_id, int cpatch_id, int zpatch_name)
         {
             OracleTransaction transaction = DBManager.BeginTransaction();
             DBManager.ExecuteNonQuery(
                 deleteDependencyScript,
                 transaction,
-                new OracleParameter("cpatch_id", cpatch_id),
+                new OracleParameter("zpatch_id", zpatch_id),
                 new OracleParameter("parent_id", parent_id),
-                new OracleParameter("release_id", release_id),
-                new OracleParameter("cpatch_name", cpatch_name));
+                new OracleParameter("cpatch_id", cpatch_id),
+                new OracleParameter("zpatch_name", zpatch_name));
             transaction.Commit();
         }
 
